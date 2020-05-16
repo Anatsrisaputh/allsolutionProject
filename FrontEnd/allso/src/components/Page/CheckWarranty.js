d5f8b6a4403b076089464722ec838d3e6f31d2c0
Anatsrisaputh <jane.stbc@gmail.com>
2020-05-17 00:21:08 +0700

    Update project

--- FrontEnd/allso/src/components/Page/CheckWarranty.js
+++ FrontEnd/allso/src/components/Page/CheckWarranty.js
@@ -1,31 +1,20 @@
 import React, { useState } from 'react';
-import axios from "../../config/axios";
+// import axios from "../../config/axios";
 import "../../style/checkwarranty.css";
-import { Input, Row, Col } from 'antd';
 
-const { Search } = Input;
+
+
 
 
 
 function CheckWarranty() {
   return (
-    <Row >
-      <Row></Row>
-      <Row  justify="middle">
-        
-        <Col span={12} offset={6}>
-        
-    
-      <Search style={{width: 500}}
-      label="Check Product warraty"
-      placeholder="Serial Number" 
-      onSearch={value => {}}
-      enterButton />
-     
-      </Col>
-      </Row>
-      <Row></Row>
-    </Row>
+    <div className="grid-container">
+      <div className="grid-item item1">
+          div item 1
+      </div>
+
+    </div>
   )
 }
 
