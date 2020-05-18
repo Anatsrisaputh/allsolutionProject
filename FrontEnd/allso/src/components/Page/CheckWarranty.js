56bd960d2ad8090cf346c7068c3d168c3b830da8
Anatsrisaputh <jane.stbc@gmail.com>
2020-05-18 21:33:35 +0700

    continues tomorrow

--- FrontEnd/allso/src/components/Page/CheckWarranty.js
+++ FrontEnd/allso/src/components/Page/CheckWarranty.js
@@ -1,26 +1,31 @@
 import React, { useState } from 'react';
-// import axios from "../../config/axios";
+import axios from "../../config/axios";
 import "../../style/checkwarranty.css";
+
 
 
 function CheckWarranty() {
 
-  const [showData, setShowData] = useState({});
+  // const [inputValue, setInputValue] = useState("");
+  // const [showData, setShowData] = useState({});
+
+  // const checkData = async axios.get()
 
   return (
     <div className="grid-container">
 
-      <div className="grid-item item2">
+      {/* <div className="grid-item item2">
         <form>
           Serial number:	&nbsp;
-          <input label="Serialnumber"type="text" id="searchBar"></input>
-          <button id="searchButton">Search</button>
+          <input label="Serialnumber"type="text" id="searchBar" 
+          value={inputValue} onChange={(event) => setInputValue(event.target.value)}></input>
+          <button id="searchButton" onClick={checkData}>Search</button>
         </form>
       </div>
 
       <div className="grid-item item3">
         Show state
-      </div>
+      </div> */}
 
     </div>
   )
