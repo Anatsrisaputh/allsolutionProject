86617f351b630528f94c3db23aacce6f6f6b5e1e
Anatsrisaputh <jane.stbc@gmail.com>
2020-05-19 23:34:36 +0700

    Try to axios.get serial number => fail

--- FrontEnd/allso/src/components/Page/CheckWarranty.js
+++ FrontEnd/allso/src/components/Page/CheckWarranty.js
@@ -6,26 +6,35 @@ import "../../style/checkwarranty.css";
 
 function CheckWarranty() {
 
-  // const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
   // const [showData, setShowData] = useState({});
 
-  // const checkData = async axios.get()
+  const onChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const showSerialNumber = async ()=> {
+   console.log(inputValue);
+   const id = inputValue;
+  const serial = await axios.get("/item/getid/", id);
+  console.log(serial)
+  }
 
   return (
     <div className="grid-container">
 
-      {/* <div className="grid-item item2">
+      <div className="grid-item item2">
         <form>
           Serial number:	&nbsp;
           <input label="Serialnumber"type="text" id="searchBar" 
-          value={inputValue} onChange={(event) => setInputValue(event.target.value)}></input>
-          <button id="searchButton" onClick={checkData}>Search</button>
+          value={inputValue} onChange={onChange}></input>
+          <button id="searchButton" onClick={showSerialNumber}>Search</button>
         </form>
       </div>
 
       <div className="grid-item item3">
-        Show state
-      </div> */}
+        {/* {showData} */}
+      </div>
 
     </div>
   )
