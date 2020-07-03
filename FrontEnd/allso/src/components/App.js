8bb74262580e4eaab179a5278735f213e9b02654
Anatsrisaputh <jane.stbc@gmail.com>
2020-07-03 09:54:03 +0700

    Go to Payoa

--- FrontEnd/allso/src/components/App.js
+++ FrontEnd/allso/src/components/App.js
@@ -9,6 +9,7 @@ import Profile from "./Page/Profile";
 import Warranty from './Page/Warranty';
 import Register from "./Page/Register";
 import CheckWarranty from './Page/CheckWarranty';
+import CreateItems from './Page/CreateItems';
 
 
 function App() {
@@ -16,21 +17,14 @@ function App() {
     <Switch>
     
       <Route exact path="/"> <Home /> </Route>
-
       <Route exact path='/cart'> <Cart /> </Route>
-
       <Route exact path="/login"> <Login /> </Route>
-
       <Route exact path="/payment"> <Payment /> </Route>   
-
       <Route exact path="/profile"> <Profile /> </Route>
-
       <Route exact path="/warranty"> <Warranty /> </Route>
-
       <Route exact path="/register"> <Register /> </Route>
-
       <Route exact path="/checkwarranty"> <CheckWarranty /> </Route>
-        
+      <Route exact path="/create-items"> <CreateItems /> </Route>
       
    
 
