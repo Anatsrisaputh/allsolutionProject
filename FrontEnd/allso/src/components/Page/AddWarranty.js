03e773233de527512245a996c4dea793cb59686e
Anatsrisaputh <jane.stbc@gmail.com>
2020-07-08 21:25:21 +0700

    add warranty form

--- FrontEnd/allso/src/components/Page/AddWarranty.js
+++ FrontEnd/allso/src/components/Page/AddWarranty.js
@@ -5,13 +5,29 @@ export default function AddWarranty() {
   return (
     <div className="create-items-main">
       <div className="create-form">
+
+        {/* Form input */}
         <form>
-          <label for="serealNumber">Sereal Number :</label> <br />
+          <label for="serialNumber">Serial Number :</label> <br />
           <input type="text" /> <br />
           
-          <label for="serealNumber">Sereal Number :</label> <br />
-          <input type="text" /> 
+          <label for="warrantyStart">Warranty Start :</label> <br />
+          <input type="date" /> <br />
 
+          <label for="warrantyPeriod">Warranty Period :</label> <br />
+          <input type="checkbox"></input> 3 &nbsp; &nbsp;
+          <input type="checkbox"></input> 5 &nbsp; &nbsp;
+          <input type="checkbox"></input> 10 &nbsp; &nbsp;
+          Years <br />
+
+          <input type="checkbox" ></input> 3 &nbsp; &nbsp;
+          <input type="checkbox"></input> 5 &nbsp; &nbsp;
+          <input type="checkbox"></input> 10 &nbsp; &nbsp;
+          Months <br />
+          
+          <button>Submit</button>
+
+          
         </form>
       </div>
     </div>
